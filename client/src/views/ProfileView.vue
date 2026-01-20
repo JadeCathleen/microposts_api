90a68fc2031ce568d4803f737d89e99f848c5ebb
JadeCathleen <cormier.jade@gmail.com>
2026-01-20 15:16:17 -0500

    Debug: logout on profile page

--- client/src/views/ProfileView.vue
+++ client/src/views/ProfileView.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { useAuth } from "../composables/useAuth"
 
-const { user } = useAuth()
+const { user, isAuthenticated } = useAuth()
 </script>
 
 <template>
-  <div>
+  <div v-if="isAuthenticated">
     <h1 class="flex items-center justify-center mb-10">Profile</h1>
-    <p>Email: {{ user.email }}</p>
-    <p>Username: {{ user.username }}</p>
+    <p>Email: {{ user?.email }}</p>
+    <p>Username: {{ user?.username }}</p>
+  </div>
+  <div v-else>
+    <h1 class="flex items-center justify-center mb-10">Profile</h1>
+    <p>Please log in to view your profile.</p>
   </div>
 </template>
